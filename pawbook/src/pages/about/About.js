2654a0395bba0ac9a761609e17a744c33990dda7
BernardoAlegria <bernas_alegria@hotmail.com>
2020-07-14 16:07:10 +0100

    fixed imports

--- pawbook/src/pages/about/About.js
+++ pawbook/src/pages/about/About.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import AuthorComponent from '../../components/author/Author';
-import './About.css';
 export default class AboutPage extends React.Component {
     render() {
         return (
             <div id="about-board">
                 <h3>Hi we're in About</h3>
-                <AuthorComponent name="Diogo" info={{ description: 'nice guy', work: 'Developer' }} />
+                
             </div>
         );
     }
