8aa23394bb26762a12ef456f6663704814e0022f
BernardoAlegria <bernas_alegria@hotmail.com>
2020-09-18 22:31:41 +0100

    add route and test page for addAnimal

--- pawbook/src/pages/animal/AddAnimal.js
+++ pawbook/src/pages/animal/AddAnimal.js
@@ -6,9 +6,10 @@ import SubmitDialogComponent from "../../components/animal/SubmitDialog";
 import SearchFormComponent from "../../components/global/SearchForm";
 
 
-export default class AnimalListPage extends React.Component {
-  //static contextType = AuthContext;
+export default class AddAnimal extends React.Component {
+  static contextType = AuthContext;
   constructor(props){
+    super(props);
     this.state={
       nome: "ze",
       tipo: "cao",
@@ -16,11 +17,10 @@ export default class AnimalListPage extends React.Component {
       raca: "labrador",
       localidade: "Setubal",
       observacoes: "qualquer coisa"
-    }
+    };
   }  
 
   componentDidMount() {
-    console.log("did mount");
     this.addAnimal();
   }
 
@@ -31,7 +31,6 @@ export default class AnimalListPage extends React.Component {
   }
 
   addAnimal(){
-    console.log("addAnimal");
     services.animal.create(this.state);
   }
 
