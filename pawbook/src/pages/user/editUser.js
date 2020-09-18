54b79d972ce9bc95aa017530315e440394290939
tia9orodri <48485537+tia9orodri@users.noreply.github.com>
2020-09-18 22:52:29 +0100

    create editUser Page

---
+++ pawbook/src/pages/user/editUser.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class UserEdit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user:""
+        };
+    }
+}
